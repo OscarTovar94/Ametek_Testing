58ff6230a960ffd8729ce641ff68cb2890529551
Oscar ELRAD <Oscar.Tovar@elrad-int.mx>
2026-09-11 10:17:55 -0600

    11/9/26 10:17

--- Ametek_Testing.py
+++ Ametek_Testing.py
@@ -500,7 +500,8 @@ class TestingGTAO:
         test_16_min = float(testspec_gtao("FPGA_MgtAVcc_Voltage", "Min"))
         test_16_max = float(testspec_gtao("FPGA_MgtAVcc_Voltage", "Max"))
 
-        continue_test = obtener_configuracion("Test", "Continue")
+        continue_test = obtener_configuracion(
+            "Test", "Continue").strip().lower() == "true"
 
         label_test1 = tk.Label(frame, text=f"Test 1: {test_1_name} - Min: {test_1_min} {test_1_unit}, Max: {test_1_max} {test_1_unit}", font=(
             "Arial", 12), fg="black")
@@ -2396,7 +2397,8 @@ class TestingGTSOC:
         test_26_min = float(testspec_gtsoc("DDR4_VTT_Voltage", "Min"))
         test_26_max = float(testspec_gtsoc("DDR4_VTT_Voltage", "Max"))
 
-        continue_test = obtener_configuracion("Test", "Continue")
+        continue_test = obtener_configuracion(
+            "Test", "Continue").strip().lower() == "true"
 
         label_test1 = tk.Label(frame, text=f"Test 1: {test_1_name} - Min: {test_1_min} {test_1_unit}, Max: {test_1_max} {test_1_unit}", font=(
             "Arial", 12), fg="black")
